0b171245844ae14a22d80edfe06324abe81fa8b6
Artur Fonseca Ferreira <115838267+artur-ferreira-dev@users.noreply.github.com>
2023-01-08 13:24:48 -0300

    Update loop_arrays.rb

---
+++ estudos/cap005/loop_arrays.rb
@@ -0,0 +1,7 @@
+prices = [3.99, 25.00, 8.99]
+
+index = 0 # começa com o indice 0
+while index < prices.length # faz um looping até chegar no fim de um array
+    puts prices[index] # acessa o elemento no índice atual.
+    index += 1 # move-se para o próximo elemento array
+end
