0cfcbaf10b8e67792700ffe26717d4a05db548a9
Artur Fonseca Ferreira <115838267+artur-ferreira-dev@users.noreply.github.com>
2023-01-09 17:25:43 -0300

    Update while_loop_arrays.rb

--- estudos/cap005/while_loop_arrays.rb
+++ estudos/cap005/while_loop_arrays.rb
@@ -13,3 +13,28 @@ end
 prices = [3.99, 25.00, 8.99]
 
 puts format("%.2f", total(prices))
+
+# dado um conjunto de preços, subtraia o saldo da conta do cliente
+def refund(prices)
+    amount = 0
+    index = 0
+    while index < prices.length
+        amount -= prices[index]
+        index += 1
+    end
+    amount
+end
+puts format("%.2f", refund(prices))
+
+# dado um conjunto de preços reduza o preço de cada item por 1/3 e imprima a economia
+
+def show_disccounts(prices)
+    index = 0
+    while index < prices.length
+        amount_off = prices[index] / 3.0
+        puts format("Your discount: $%.2f",amount_off)
+        index += 1
+    end
+end
+
+show_disccounts(prices)
