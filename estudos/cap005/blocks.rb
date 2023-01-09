a004511d6a9e8f4096bcf6dbb2fb96f5a432fca0
Artur Fonseca Ferreira <115838267+artur-ferreira-dev@users.noreply.github.com>
2023-01-09 18:15:57 -0300

    Update blocks.rb

--- estudos/cap005/blocks.rb
+++ estudos/cap005/blocks.rb
@@ -3,3 +3,36 @@ def my_method(&my_block) # este método aceita um bloco como parâmetro &, o rub
     my_block.call # o método de chamar o bloco
     puts "We're back in the method!"
 end
+ 
+my_method do 
+    puts "We're in the block!" # o bloco que será armazenado em my_block
+end
+
+my_method do
+    puts "It's a block party!" # o código do bloco pode ser alterado quantas vezes forem necessárias
+end
+
+def twice(&my_block)
+    puts "In the method, about to call the block!"
+    my_block.call # chama o bloco
+    puts "Back in the method, about to call the block again!"
+    my_block.call # chama o bloco novamente
+    puts "Back in the method, about to return!"
+end
+
+twice do
+    puts "Woooo!"
+end
+
+# Parâmetros de bloco
+
+def give(&my_block)
+    my_block.call("2 turtle doves", "1 partridge")
+end
+# É possível que um bloco aceite um ou mais parâmetros de método. definindo-os entre ||
+give do |present1, present2| # parametros que receberão os parametros definidos no metodo de chamada
+    puts "My method gave to me..."
+    puts present1, present2
+end
+
+
